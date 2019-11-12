cb4651e9aae1d2d77be3745f642147d9f11fed72
eunyoungroh <roheunyoung@outlook.com>
2019-11-12 15:44:41 +0900

    실습 3

--- 11_pointer_2/11_pointer_2/Source.c
+++ 11_pointer_2/11_pointer_2/Source.c
@@ -2,14 +2,19 @@
 
 int main(void) {
 
-	int num = 10;
-	int* ptr = &num;
+	char buffer[8];
+	double* pd;
+	int* pi;
 
-	printf("num = %d, pi : %p\n", num, ptr);
-	(*ptr)++;
-	printf("num = %d, pi : %p\n", num, ptr);
-	*ptr++;
-	printf("num = %d, pi : %p\n", num, ptr);
+	pd = (double*)buffer;
+	*pd = 3.14;
+	printf("%f\n", *(double*)buffer);
+
+	pi = (int*)buffer;
+	*pi = 123;
+	*(pi + 1) = 456;
+	printf("%d %d\n", *(int*)buffer, *((int*)buffer + 1));
 
 	return 0;
+
 }
