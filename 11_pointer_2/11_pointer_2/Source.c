d15589505c56e2379eca99445e6e4c60564b17ff
eunyoungroh <roheunyoung@outlook.com>
2019-11-12 15:30:01 +0900

    실습 2

--- 11_pointer_2/11_pointer_2/Source.c
+++ 11_pointer_2/11_pointer_2/Source.c
@@ -2,20 +2,14 @@
 
 int main(void) {
 
-	char* pc;
-	int* pi;
-	double* pd;
-	pc = (char*)10000;
-	pi = (int*) 10000;
-	pd = (double*)10000;
+	int num = 10;
+	int* ptr = &num;
 
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-
-	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	printf("num = %d, pi : %p\n", num, ptr);
+	(*ptr)++;
+	printf("num = %d, pi : %p\n", num, ptr);
+	*ptr++;
+	printf("num = %d, pi : %p\n", num, ptr);
 
 	return 0;
 }
