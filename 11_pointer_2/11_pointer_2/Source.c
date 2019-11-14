860162808364548e30c188703b201ebda411d5f9
eunyoungroh <roheunyoung@outlook.com>
2019-11-14 15:46:05 +0900

    실습 5_주석추가

--- 11_pointer_2/11_pointer_2/Source.c
+++ 11_pointer_2/11_pointer_2/Source.c
@@ -5,14 +5,21 @@ void setPointer(char** q);
 
 int main(void) {
 
-	char* p = "zzz";
-	setPointer(&p);
-	printf("%s\n", p);
+	char* ptr = "zzz";
+
+	// 포인터 변수의 주소를 넘김
+	setPointer(&ptr);
+	printf("%s\n", ptr);
 
 	return 0;
 }
 
-void setPointer(char** q)
+void setPointer(char** address_of_ptr)
 {
-	*q = proverb;
+	// 포인터 변수의 주소를 de-referecing하면
+	// 그 포인터가 담고 있는 값이 나옴
+	printf("%s\n", *address_of_ptr);
+
+	// 그 포인터가 담고 있는 값에 proverb를 대입
+	*address_of_ptr = proverb;
 }
